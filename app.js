ab82c4ed09a29010b3503200feb578db0e362b8a
Hugo Luna <hugo94ronal@gmail.com>
2019-01-22 18:36:02 -0600

    Homework done

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const http = require('http')
+const port = 3002
 const fs = require('fs')
 
-app.get('/', (req, res) => {
-  res.sendfile('index.html')
+app.get('/', (req, res, next) => {
+  fs.readFile(`${__dirname}/index.html`, "utf8", (err, data) => {
+    res.send(data)
+  })
+})
+
+//Handle 404
+app.use((req, res, next) => {
+    res.status(404).send({message:"404. Not Found"})
+    next()
+  });
+
+//Handle 500
+app.use((req, res, next) => {
+  return res.status(500).send({message:"500. Server Error"})
 })
 
 app.listen(port, () => {
-  console.log("Running in port 3000")
+  console.log("Running in port 3001")
 })
